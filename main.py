b8c7bd1c540ebf338bb7a75ddcfd75bea84b0a88
fmarcabru <onin@protonmail.com>
2021-12-21 23:36:18 +1100

    adding several demo scenarios

--- main.py
+++ main.py
@@ -1,81 +1,94 @@
 import driver
+import uping
 from utime import sleep_ms
-# from utime import ticks_ms, ticks_diff
-#import OpenSansBold as font
-#import OpenSansBoldDigits20 as digits20
-import OpenSansBoldDigits20 as digits30
-#import OpenSansBoldDigits39 as digits40
-#import OpenSansBoldDigits as digits50
-#from micropython import mem_info
-#import antonio20 as font20
+from micropython import mem_info
+import usys as sys
 
-
-print(spi)
-#print("here is the memory {}".format(mem_info(True)))
-epaper = driver.EPaperDisplay(rst, dc, busy, cs, spi)
-epaper.init()
-epaper.clear()
-frame_black = epaper.setframe()
-print("here")
-frame_red = epaper.setframe()
-print("Frame set!")
-#print("here is the memory {}".format(mem_info(True)))
-#start=ticks_ms()
-epaper.draw_bmp_at(frame_black,120,10,'icon.bmp',driver.COLORED)
-#end=ticks_ms()
-#epaper.draw_filled_circle(frame_red, 10, 10, 25, driver.COLORED)
-#print("Load image time: {}".format(ticks_diff(end,start))) 
-
-
-xpos=30
-ypos=50
-
-#for i in range(1,8):
-#    for j in range(-1,2):
-#        epaper.draw_vertical_line(frame_black,(xpos+j)*i+font.width,ypos,font.height,driver.COLORED)
-
-
-#for i in range(3):
-#    epaper.draw_horizontal_line(frame_black,xpos,ypos-i,font.width*7,driver.COLORED)
-
-#for i in range(3):
-#    epaper.draw_horizontal_line(frame_black,xpos,ypos+i+font.height,font.width*7,driver.COLORED)
-
-#print(type(font))
-
-epaper.display_str(frame_black, 120, ypos, "what is here", digits30, driver.COLORED)
-xpos=30
-ypos=120
-epaper.display_str(frame_black, 10, 90, "1234", digits30, driver.COLORED)
-epaper.display_str(frame_black, 10, 10, "1234", digits30, driver.COLORED)
-epaper.display_str(frame_black, 10, 40, "1234", digits30, driver.COLORED)
-#epaper.display_str(frame_red, 10, 90, "1234", digits40, driver.COLORED)
-epaper.display_str(frame_black, 10, 150, "1234", digits30, driver.COLORED)
-#epaper.display_string_at(frame_black, 30, 30, "More 45", font, driver.COLORED)
-
-
-#epaper.draw_vertical_line(frame_black,99,100,font.height,driver.COLORED)
-#epaper.draw_vertical_line(frame_black,100,100,font.height,driver.COLORED)
-#epaper.draw_vertical_line(frame_black,100+font.width,100,font.height,driver.COLORED)
-#epaper.draw_vertical_line(frame_black,101+font.width,100,font.height,driver.COLORED)
-#epaper.draw_horizontal_line(frame_black,100,100,font.width,driver.COLORED)
-#epaper.draw_horizontal_line(frame_black,100,99,font.width,driver.COLORED)
-#epaper.draw_horizontal_line(frame_black,100,100+font.height,font.width,driver.COLORED)
-#epaper.draw_horizontal_line(frame_black,100,101+font.height,font.width,driver.COLORED)
-
-#epaper.display_string_at(frame_black, 100, 100, "M", font, driver.COLORED)
-
-
-epaper.display_frame(frame_black, frame_red)
+import firasansbold25capsnum as example_font
 
 
 
+def display_image(epaper: driver.EPaperDisplay, image: str, frame: str)-> None:
+    epaper.draw_bmp('black',"frame2.bmp",driver.COLORED)
 
-#print("FONT DATA TYPE {}".format(type(font.data)))
-print("finish")
-##epaper.sleep() 
-sleep_ms(10000)
-epaper.init()
-epaper.clear()
-epaper.sleep() 
-print("last")
+
+def prepare_display(rst: Pin, dc: Pin, busy: Pin, cs: Pin, spi: SPI):
+    print(spi)
+    epaper = driver.EPaperDisplay(rst, dc, busy, cs, spi)
+    epaper.rotate=driver.ROTATE_270
+    epaper.init()
+    epaper.clear()
+    epaper.sleep()
+    return (epaper)
+
+
+
+def send_to_display(display: driver.EPaperDisplay):
+    print("init display")
+    display.init()
+    print("clearing display")
+    display.clear()
+    print("printing frame")
+    display.display_frame()
+    print("sleeping")
+    display.sleep()
+
+try:
+    #mem_info(True)
+    epaper = prepare_display(rst, dc, busy, cs, spi)
+
+    while True:
+        #show bmp 
+        epaper.draw_bmp('black',"black_test.bmp",driver.COLORED)
+        epaper.draw_bmp('red', 'red_test.bmp', driver.COLORED)
+        send_to_display(epaper)
+        sleep_ms(10000)
+
+        #show text
+        epaper.clear_canvas('black')
+        epaper.clear_canvas('red')
+        epaper.display_str("black", 20, 20, "12345", example_font, colored=driver.COLORED)
+        epaper.display_str("red",20, 100, "AABBCC", example_font, colored=driver.COLORED)
+        send_to_display(epaper)
+        sleep_ms(10000)
+
+        #show circle
+        epaper.clear_canvas('black')
+        epaper.clear_canvas('red')
+        epaper.draw_filled_circle('black', 20, 30, 20, driver.COLORED)
+        epaper.draw_filled_circle('red', 100, 100, 30, driver.COLORED)
+        epaper.draw_circle('black', 100, 100, 50, driver.COLORED)
+        epaper.draw_circle('black', 100, 100, 51, driver.COLORED)
+        send_to_display(epaper)
+        sleep_ms(10000)
+
+        #show rectangle
+        epaper.clear_canvas('black')
+        epaper.clear_canvas('red')
+        epaper.draw_filled_rectangle('black', 10, 10, 90, 90, driver.COLORED)
+        epaper.draw_filled_rectangle('red', 110, 110, 190, 190, driver.COLORED)
+        epaper.draw_rectangle('black', 5, 5, 195,195, driver.COLORED)
+        send_to_display(epaper)
+
+
+        sleep_ms(10000)
+
+        
+
+    
+
+
+except Exception as e:
+#    mem_info(True)
+    print(sys.print_exception(e))
+    epaper.init()
+    epaper.clear()
+    epaper.sleep()
+
+except KeyboardInterrupt:
+    print("Interrupt detected")
+    epaper.init()
+    epaper.clear()
+    epaper.sleep()
+   
+    
